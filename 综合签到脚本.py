920bbf867d31e8c32877eb985dca56128d4e0959
ultramanzhang <1627825020@qq.com>
2023-01-19 09:37:10 +0800

    修改综合签到脚本编译BUG

--- 综合签到脚本.py
+++ 综合签到脚本.py
@@ -2,16 +2,15 @@ import time
 import requests
 import os
 from bs4 import BeautifulSoup
-from multiprocessing import Process
-
+from threading import Thread
 
 
 def xiaolonghui():
     a = int(time.time() * 1000)
-    url = 'https://qualcomm.growthideadata.com/qualcomm-app/api/user/signIn?userId='
-    headers = {'sessionkey': '',
+    url = 'https://qualcomm.growthideadata.com/qualcomm-app/api/user/signIn?userId=290543'
+    headers = {'sessionkey': '21PbeedREFL6U5bZ85j6jQ==',
                'User-Agent': 'Mozilla/5.0 (Linux; Android 12; 2206122SC Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4375 MMWEBSDK/20221109 Mobile Safari/537.36 MMWEBID/4835 MicroMessenger/8.0.31.2281(0x28001F37) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android',
-               'userid': '',
+               'userid': '290543',
                'timestamp': str(a),
                }
     response = requests.get(url=url, headers=headers)
@@ -30,9 +29,9 @@ def xiaolonghui():
 def bugku():
     url = "https://ctf.bugku.com/user/checkin"
     headers = {
-        "cookie": ""
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-        # "x-csrf-token": ""
+        "cookie": "Hm_lvt_97426e6b69219bfb34f8a3a1058dc596=1673667909; PHPSESSID=5437a83dba46f8f989fc85f69db4d7ea; autoLogin=k91a6oRDiH82hQQsxG98rnS9Z0dSe7yDQKuOQQaMjN4RCuAQkGMdGTGaWMD8%2B3gx4EftvyjxOv7Bz5Jab%2BGP07ZtTFEVHy38DlEaD60mkxwEZCJ8rNMb1BfiNg2WsgXqUgludbEVzG2n; X-CSRF-TOKEN=e296704a1ab19bb54b383fb4b533803f",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        # "x-csrf-token": "4a75a08a9d6c66f8d3ac3a597d339fd2"
     }
     res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
@@ -42,9 +41,9 @@ def bugku():
 
 
 if __name__ == '__main__':
-    Process(target=xiaolonghui).start()#启动多进程
-    Process(target=bugku).start()
+    Thread(target=xiaolonghui).start()#启动多进程
+    Thread(target=bugku).start()
     # 扩展空间，方便以后增加新的签到函数
-
+    
 
     os.system('pause')
